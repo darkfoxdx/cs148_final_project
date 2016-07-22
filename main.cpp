1d7fb6b1cddb172367aa1347893e093e407b6d13
Eugene Low <eugene.ws.low@gmail.com>
2016-07-22 15:24:50 -0700

    Second push

--- main.cpp
+++ main.cpp
@@ -57,6 +57,9 @@ int main()
         glfwPollEvents();
 
         // Rendering commands here
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         // Swap the buffers
         glfwSwapBuffers(window);
